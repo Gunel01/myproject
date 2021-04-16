53ccd973f9d9d63b43a04469c770b7cc9ed82135
Gunel01 <82421807+Gunel01@users.noreply.github.com>
2021-04-16 00:18:06 -0400

    Update SWSteps.java

--- Project01/src/test/java/api/stepDefinitions/SWSteps.java
+++ Project01/src/test/java/api/stepDefinitions/SWSteps.java
@@ -2,7 +2,10 @@ package api.stepDefinitions;
 
 
 	import com.jayway.jsonpath.JsonPath;
-	import io.cucumber.java.en.And;
+
+import io.cucumber.core.api.Scenario;
+import io.cucumber.java.Before;
+
 	import io.cucumber.java.en.Given;
 	import io.cucumber.java.en.Then;
 	import io.restassured.RestAssured;
@@ -10,55 +13,70 @@ package api.stepDefinitions;
 
 	import static io.restassured.RestAssured.given;
 	import static org.junit.Assert.assertEquals;
-	import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
 	public class SWSteps {
-	    Response response;
+	 	Response response;
 	    String requestData;
 	    List<Object>SWCharacters;
-
-	    @Given("User submits GET request for a list of all the Star Wars characters and receive response")
-	    public void SW_Characters_GETRequest() {
-	        RestAssured.baseURI="https://swapi.dev/api/people/1/";
-	        response= given()
-	                .header("Content-Type","application/json")
-	                .body(requestData)
-	                .relaxedHTTPSValidation()
-	                .when()
-	                .post();
-	        response.prettyPrint();
-
-	    }
-	    @And("User should be able to return a list of all the Star Wars characters")
-		public void user_retrieves_Star_Wars_characters_from_response_and_add_them_to_a_List() {
-			
-			SWCharacters=JsonPath.read(response.asString(),"$.results[*]");
-			System.out.println("*****************************************");
-			System.out.println(SWCharacters.size());
-			System.out.println("*****************************************");
-	    }
-
-	    @Then("User validates if status code is {int}")
-		public void user_validates_if_status_code_is(int statusCode) {
-			System.out.println("*****************************************");
-			assertEquals(statusCode, response.getStatusCode());
-			System.out.println("*****************************************");
+	
+	    Scenario scenario;
+		
+		@Before
+		public void keepScenario(Scenario scenario) {
+			this.scenario = scenario;
 		}
-		@And("User validates if the count of the characters in response is the same as required.")
-		public void user_validates_if_the_count_of_the_characters_in_response_is_the_same_as_required() {
-			
-			int actualResult = SWCharacters.size();
-			int expectedResult = 10;
-			assertEquals(actualResult, expectedResult);
-			
-			
-			
-			
+	   
+	@Given("User submits GET request for a list of all the Star Wars characters")
+	public void user_submits_GET_request_for_a_list_of_all_the_Star_Wars_characters() {
+		
+		        RestAssured.baseURI="https://swapi.dev/api/people";
+		        response=given()
+		        		.header("Content-Type","application/json")
+		                .relaxedHTTPSValidation()
+		                .when()
+		                .get();
+		        response.prettyPrint();
+		       
+		        scenario.write("Response: " + response.prettyPrint());
+		       
+		        scenario.embed(response.asString().getBytes(), "application/json");
+		       
+		       
+		    }
+	
+	@Given("User retrieves Star Wars characters from response and add them to a List")
+	public void user_retrieves_Star_Wars_characters_from_response_and_add_them_to_a_List() {
+		
+		SWCharacters=JsonPath.read(response.asString(),"$.results[*]");
+		System.out.println("*****************************************");
+		System.out.println(SWCharacters.size());
+		System.out.println("*****************************************");
+		
+		
+		scenario.write("Characters: " + SWCharacters);
+    }
+	  
+	@Then("user validates if status code is {int}")
+	public void user_validates_if_status_code_is(int statusCode) {
+		System.out.println("*****************************************");
+		assertEquals(statusCode, response.getStatusCode());
+		System.out.println("*****************************************");
+		
+		scenario.write("Status Code: " + statusCode);
+	}
+	@Then("user validates if the count of the characters in response is the same as required.")
+	public void user_validates_if_the_count_of_the_characters_in_response_is_the_same_as_required() {
+		
+		int actualResult = SWCharacters.size();
+		int expectedResult = 10;
+		
+		assertEquals(actualResult, expectedResult);
+		
+		scenario.write("Assertion actualResult" + actualResult);
+		scenario.write("Assertion expectedResult" + expectedResult);
 	    }
 
 	   
 	}
-
-
